7f2d432d681f6a41f0c6ecec73c18c5daae78d60
Artem <temmkainc@gmail.com>
2025-11-14 19:52:04 +0100

    add: firebase setup

--- application/android/app/build.gradle.kts
+++ application/android/app/build.gradle.kts
@@ -42,3 +42,16 @@ android {
 flutter {
     source = "../.."
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+apply(plugin = "com.google.gms.google-services")
