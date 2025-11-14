ac533352d43d05ac0d57fbe6ac636940df36039c
Kitava <malynovskydmitriyy@gmail.com>
2025-11-14 20:27:04 +0100

    refactor: integrate authentication flow with Pinia auth store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,51 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+import Login from './pages/Login.vue'
+import Register from './pages/Register.vue'
 import MainView from './components/MainView.vue'
-import { ref } from 'vue'
-import Login from './components/Login.vue'
-import Dashboard from './components/dashboard/Dashboard.vue'
+import Header from './components/Header.vue'
 
-const isLoggedIn = ref(false)
+const authStore = useAuthStore()
+const showRegister = ref(false)
 
 const handleLogin = () => {
-  isLoggedIn.value = true
-  console.log('User logged in!')
+  showRegister.value = false
 }
+
+const handleRegister = () => {
+  showRegister.value = false
+}
+
+const handleLogout = async () => {
+  await authStore.logout()
+  showRegister.value = false
+}
+
+const goToRegister = () => {
+  showRegister.value = true
+}
+
+const goToLogin = () => {
+  showRegister.value = false
+}
+
+onMounted(async () => {
+  await authStore.initializeAuth()
+})
 </script>
 
 <template>
   <v-app>
+    <!-- Show Header only when logged in -->
+    <Header v-if="authStore.isLoggedIn" :is-logged-in="authStore.isLoggedIn" @logout="handleLogout" />
+    
     <v-main>
-      <Login v-if="!isLoggedIn" @login="handleLogin" />
-
-      <div v-else>
-        <MainView />
-      </div>
+      <template v-if="!authStore.isLoggedIn">
+        <Register v-if="showRegister" @register="handleRegister" @go-to-login="goToLogin" />
+        <Login v-else @login="handleLogin" @go-to-register="goToRegister" />
+      </template>
+      <MainView v-else />
     </v-main>
   </v-app>
 </template>
